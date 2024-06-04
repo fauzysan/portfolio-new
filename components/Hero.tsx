c9e6e0c3d0e819b869944d01c4bb90456cae3d78
Fauzy Gaming <fauzygaming47@gmail.com>
2024-06-05 00:00:14 +0700

    update Responsive mobile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,23 +16,27 @@ const Hero = () => {
   ]
   return (
 
-    <div className='flex lg:flex-row max-md:flex-col-reverse sm-md:text-center items-center justify-between font-sans py-52 h-screen w-full dark:bg-grid-white/[0.05] bg-grid-black/[0.2] '>
+    <div className='flex lg:flex-row max-md:flex-col-reverse sm-md:text-center items-center justify-between font-sans h-screen w-full dark:bg-grid-white/[0.05] bg-grid-black/[0.2] max-md:mb-64'>
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
         />
-        <div className='mx-auto max-md:mt-24 flex flex-col'>
-            <p className='font-semibold text-2xl lg:text-4xl my-2'>Hello, I'm <br /></p>
+        <Spotlight
+        className="top-40 -left-0 md:-left-60 md:top-20"
+        fill="white"
+        />
+        <div className='mx-auto flex flex-col'>
+            <p className='font-semibold text-2xl lg:text-4xl'>Hello, I'm <br /></p>
             <TypewriterEffectSmooth words={word}/>
-            <p className='my-10 text-xl'>Based in Indonesia. I'm fullstack developer passionate <br/>about building a modern web application.</p>
-            <Link href={"mailto:fauzyhafidz123@gmail.com"} className='inline-block'>
+            <p className='text-xl'>Based in Indonesia. I'm fullstack developer passionate <br/>about building a modern web application.</p>
+            <Link href={"mailto:fauzyhafidz123@gmail.com"} className='inline-block my-5'>
               <Button className='hover:text-slate-400'>
                 <h1 className='text-2xl font-bold'>Contact Me</h1>
               </Button>
             </Link>
         </div>
-        <div className='mx-auto w-[450px] h-[480px] max-md:size-64'>
+        <div className='mx-auto w-[450px] h-[480px] max-md:size-64 max-md:my-24'>
             <img src={Fauzi.src} alt="fauzi"  className='rounded-full'/>
         </div>  
     </div>
