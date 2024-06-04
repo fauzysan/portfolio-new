1c0773373d6e3e12afec74b25295e3de87b27af8
Fauzy Gaming <fauzygaming47@gmail.com>
2024-06-05 00:18:56 +0700

    update Card Project Responsive mobile

--- components/ui/3d-card.tsx
+++ components/ui/3d-card.tsx
@@ -86,7 +86,7 @@ export const CardBody = ({
   return (
     <div
       className={cn(
-        "h-96 w-96 max-md:w-full [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
+        "h-96 w-96 max-md:w-[400px] [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
         className
       )}
     >
